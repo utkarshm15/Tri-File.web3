f485024494762852880c16ee540757c99fb21afb
Utkarsh Mishra <113801191+utkarshm15@users.noreply.github.com>
2024-04-30 23:10:38 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,95 @@
+import Upload from "./artifacts/contracts/Upload.sol/Upload.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import FileUpload from "./components/FileUpload";
+import Display from "./components/Display";
+import Modal from "./components/Modal";
+import "./App.css";
+
+function App() {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Upload.abi,
+          signer
+        );
+        //console.log(contract);
+        setContract(contract);
+        setProvider(provider);
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+    provider && loadProvider();
+    const bgAnimation = document.getElementById('bgAnimation');
+
+const numberOfColorBoxes = 400;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.appendChild(colorBox);
+}
+  }, []);
+  return (
+    <>
+      
+      
+
+      <div className="App">
+     
+      <div className="bgAnimation" id="bgAnimation">
+        <div className="backgroundAmim">
+            
+        </div>
+    </div>
+        <div className="apptext">
+        {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
+        {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+        <h1 > Tri-File<span >.web3</span></h1>
+        
+        <p style={{ color: "white" }}>
+         <span> Account :</span> {account ? account : "Not connected"}
+        </p>
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        <Display contract={contract} account={account}></Display>
+      </div>
+      </div>
+    </>
+  );
+}
+
+
+export default App;
